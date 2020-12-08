0bfa51b56fc07c98892c4ba171edd143eece23a0
AkshayKJha <jha.akshay89@gmail.com>
2020-12-08 01:10:07 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup (
+    name= "component1"
+    version= "1.0.0"
+    description="",
+    author="",
+    author_email="",
+    url="",
+    package_dir={"":"src"},
+    packages=[''],
+    classifiers= ['Programming Language :: Python'])
